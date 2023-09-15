d5daa3ada4979d67b8c6c73a310198ec00613e19
Gokul-nath-K <gokulnathk003@gmail.com>
2023-09-15 10:20:06 +0530

    two component added

--- phone_book/src/App.js
+++ phone_book/src/App.js
@@ -1,12 +1,27 @@
 import './Assets/CSS/App.css'
+import Register from './Pages/Register';
+import './Assets/CSS/App.css'
+import Home from './Components/Home';
+import { Route, Routes } from 'react-router-dom';
+import { userContext } from './Assets/Contexts/UserContext';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [user, setUser] = useState({});
+  
+  const contextValue = { user, setUser};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
+    <>
+    <userContext.Provider value={contextValue} >
+      <Routes>
+          <Route exact path='/' element={<Register/>}/>
+          <Route exact path='/home' element={<Home/>}/>
+        </Routes>
+    </userContext.Provider>
+    </>
   );
 }
 
